1571f2aa6546d7d5811abfb929e8fcf46fbba92e
Jesus Coello <jescoevas@hotmail.com>
2020-09-21 21:54:11 +0200

    Buscador hecho

--- assets/js/app.js
+++ assets/js/app.js
@@ -31,6 +31,34 @@ let cargarTodos = () => {
     });
 }
 
+$("#search").keyup(() => {
+    let val = $("#search").val()
+    $.post('backend/search.php', { val }, (data, status) => {
+        let articulos = JSON.parse(data)
+        let template = ''
+        articulos.forEach(art => {
+            template += `
+                <div id="${art.id}" class="card mb-2">
+                    <div class="card-header">
+                        ${art.titulo}
+                    </div>
+                    <div class="card-body">
+                        <blockquote class="blockquote mb-0">
+                            <p>${art.cuerpo}</p>
+                            <footer class="blockquote-footer">
+                                ${art.autor}
+                                <i class="fa fa-pencil mx-1" onclick="editar(${art.id},'${art.autor}','${art.titulo}','${art.cuerpo}')"></i>
+                                <i class="fa fa-trash mx-1" onclick="eliminar(${art.id})"></i>
+                            </footer>
+                        </blockquote>
+                    </div>
+                </div>
+                `
+        });
+        $("#articulos").html(template)
+    });
+})
+
 $("#nuevo").click(() => $("#form").toggle(500))
 
 let editar = (id, autor, titulo, cuerpo) => {
@@ -69,10 +97,7 @@ $("#form").submit((e) => {
         }
         $.post('backend/create.php', data, (resp) => {
             cargarTodos()
-            $("#id").val(null)
-            $("#autor").val("")
-            $("#titulo").val("")
-            $("#cuerpo").val("")
+            vaciar()
             $("#form").toggle(500)
         });
     } else {
@@ -84,12 +109,8 @@ $("#form").submit((e) => {
         }
         $.post('backend/update.php', data, (resp) => {
             cargarTodos()
-            $("#id").val(null)
-            $("#autor").val("")
-            $("#titulo").val("")
-            $("#cuerpo").val("")
+            vaciar()
             $("#form").toggle(500)
         });
     }
-
 })
---
+++ backend/search.php
@@ -0,0 +1,19 @@
+<?php
+
+include('database.php');
+
+$val = $_POST['val'];
+
+$sql = "SELECT * FROM ARTICULOS WHERE TITULO LIKE '%$val%'";
+$result = mysqli_query($connection, $sql);
+if(!$result){
+    die('Error en la consulta -> '.mysqli_error($connection));
+}
+$json = array();
+while($row = mysqli_fetch_array($result)){
+    $json[] = array('id' => $row['id'],'autor' => $row['autor'], 'titulo' => $row['titulo'], 'cuerpo' => $row['cuerpo']);
+}
+$jsonstring = json_encode($json);
+echo $jsonstring;
+
+?>
